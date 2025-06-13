089e73a1d2681a7af11bb4ad893c32ec501a7329
Thereache <thereache2003@gmail.com>
2025-06-13 07:38:37 +0000

    Revert "redwood: Import ant+ blob"
    This reverts commit acad2f4e20da901d805262926538ab3a063b4019.
    
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- redwood-vendor.mk
+++ redwood-vendor.mk
@@ -762,7 +762,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl-qti \
     camera.qcom \
     camera.xiaomi \
-    com.dsi.ant@1.0-impl \
     com.qti.chi.override \
     displayfeature.default \
     fingerprint.goodix.default \
